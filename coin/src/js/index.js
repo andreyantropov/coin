63602aae0792c6f5255acc715882cdabea46d179
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-06-07 15:55:56 +0500

    navigo-page-refresh-fix

--- coin/src/js/index.js
+++ coin/src/js/index.js
@@ -9,24 +9,10 @@ import createAccountsSectionView from './modules/create-accounts-section-view.js
 import createMapSectionView from './modules/create-map-section-view.js';
 import createAccountDataSectionView from './modules/create-account-data-section-view.js';
 
-const router = new Navigo(null, true);
+const router = new Navigo('/', { linksSelector: 'a', hash: true });
 
 const nav = document.getElementById('nav');
-const banksBtn = document.getElementById('banks-btn');
-const logoutBtn = document.getElementById('logout-btn');
 const main = document.getElementById('main');
-
-banksBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  router.navigate('/banks');
-});
-logoutBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  API.logout();
-  router.navigate('/auth');
-});
 
 const checkAuth = (done) => {
   const isAuthenticated = localStorage.getItem('coin-auth-token');
@@ -49,78 +35,95 @@ const checkNotAuth = (done) => {
 };
 
 router
-  .on('/', () => {
-    checkAuth(async () => {
-      nav.style.display = 'block';
-      main.innerHTML = '';
+  .on({
+    '/': () => {
+      checkAuth(async () => {
+        nav.style.display = 'block';
+        main.innerHTML = '';
 
-      let accountList = await API.getAccountList();
-      createAccountsSectionView(main, accountList, {
-        onAccauntBtnClick: (id) => { router.navigate(`/accounts/${id}`); },
-        onSortSelectChange: (sortOption) => {
-          switch (sortOption) {
-            case 'По номеру':
-              accountList.sort((a, b) => a.account - b.account);
-              break;
-            case 'По балансу':
-              accountList.sort((a, b) => a.balance - b.balance);
-              break;
-            case 'По последней транзакции':
-              accountList.sort((a, b) => {
-                if (!a.transactions[0] && !b.transactions[0]) {
-                  return 0;
-                } else if (!a.transactions[0]) {
-                  return -1;
-                } else if (!b.transactions[0]) {
-                  return 1;
-                } else {
-                  return new Date(a.transactions[0].date) - new Date(b.transactions[0].date);
-                }
-              });
-              break;
-            default:
-              break;
-          }
-        },
-        onNewBtnClick: async () => {
-          const newAccount = await API.createAccount();
-          accountList.push(newAccount);
-        },
+        let accountList = await API.getAccountList();
+        createAccountsSectionView(main, accountList, {
+          onAccauntBtnClick: (id) => {
+            router.navigate(`/accounts/${id}`);
+          },
+          onSortSelectChange: (sortOption) => {
+            switch (sortOption) {
+              case 'По номеру':
+                accountList.sort((a, b) => a.account - b.account);
+                break;
+              case 'По балансу':
+                accountList.sort((a, b) => a.balance - b.balance);
+                break;
+              case 'По последней транзакции':
+                accountList.sort((a, b) => {
+                  if (!a.transactions[0] && !b.transactions[0]) {
+                    return 0;
+                  } else if (!a.transactions[0]) {
+                    return -1;
+                  } else if (!b.transactions[0]) {
+                    return 1;
+                  } else {
+                    return (
+                      new Date(a.transactions[0].date) -
+                      new Date(b.transactions[0].date)
+                    );
+                  }
+                });
+                break;
+              default:
+                break;
+            }
+          },
+          onNewBtnClick: async () => {
+            const newAccount = await API.createAccount();
+            accountList.push(newAccount);
+          },
+        });
       });
-    });
-  })
-  .on('/accounts/:id', (data) => {
-    checkAuth(async () => {
-      const id = data.data.id;
+    },
+    '/accounts/:id': ({ data }) => {
+      checkAuth(async () => {
+        const id = data.id;
 
-      nav.style.display = 'block';
-      main.innerHTML = '';
-      createAccountDataSectionView(main, await API.getAccount(id), { 
-        onBackBtnClick: () => {
-          router.navigate('/');
-        },
-        onTransactionsTableClick: () => {
-          alert('Я пока еще не написан');
-        }
-       })
-    });
-  })
-  .on('/banks', () => {
-    checkAuth(async () => {
-      nav.style.display = 'block';
-      main.innerHTML = '';
-      createMapSectionView(main, await API.getBankList());
-    });
-  })
-  .on('/auth', () => {
-    checkNotAuth(() => {
-      nav.style.display = 'none';
-      main.innerHTML = '';
-      createAuthFormView(main, {
-        onAuthSubmit: async (login, password) => {
-          if (await API.login(login, password)) router.navigate('/');
-        },
+        nav.style.display = 'block';
+        main.innerHTML = '';
+        createAccountDataSectionView(main, await API.getAccount(id), {
+          onBackBtnClick: () => {
+            router.navigate('/');
+          },
+          onTransactionsTableClick: () => {
+            router.navigate(`/account/${id}/history`);
+          },
+        });
       });
-    });
+    },
+    '/accounts/:id/history': ({ data }) => {
+      checkAuth(async () => {
+        const id = data.id;
+        console.log(id);
+      });
+    },
+    '/banks': () => {
+      checkAuth(async () => {
+        nav.style.display = 'block';
+        main.innerHTML = '';
+        createMapSectionView(main, await API.getBankList());
+      });
+    },
+    '/auth': () => {
+      checkNotAuth(() => {
+        nav.style.display = 'none';
+        main.innerHTML = '';
+        createAuthFormView(main, {
+          onAuthSubmit: async (login, password) => {
+            if (await API.login(login, password)) router.navigate('/');
+          },
+        });
+      });
+    },
+    '/logout': () => {
+      API.logout();
+      router.navigate('/auth');
+    },
   })
   .resolve();
--- coin/webpack.config.js
+++ coin/webpack.config.js
@@ -85,5 +85,6 @@ module.exports = {
     compress: true,
     port: 9000,
     open: true,
+    historyApiFallback: true,
   },
 };
