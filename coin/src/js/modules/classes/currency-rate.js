d8515b002c6eacb9180aaefd28a4ab9e2c939f4d
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-06-11 17:16:42 +0500

    icons

--- coin/src/js/modules/classes/currency-rate.js
+++ coin/src/js/modules/classes/currency-rate.js
@@ -26,13 +26,30 @@ export default class CurrencyRate {
   }
 
   createElement() {
+    const use = document.createElementNS("http://www.w3.org/2000/svg", 'use');
+    use.setAttributeNS("http://www.w3.org/1999/xlink", 'xlink:href', this.getIcon());
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+    svg.classList.add('icon_rate-arrow');
+    svg.appendChild(use);
+
     const tr = el('tr', { class: 'dictionary__tr currency' }, [
       el('th', `${this._from}/${this._to}`, { class: 'dictionary__th currency__th' }),
       el('td', { class: 'dictionary__td currency__td' }, [
         el('span', formatMoney(this._rate)),
-        el('span', this._change),
+        svg,
       ]),
     ]);
     return tr;
+  }
+
+  getIcon() {
+    switch(this._change) {
+      case -1:
+        return './img/sprite.svg#sprite-arrow-down';
+      case 1:
+        return './img/sprite.svg#sprite-arrow-up';
+      default:
+        return '';
+    }
   }
 }
