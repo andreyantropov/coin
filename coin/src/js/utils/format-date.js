52f2debe441e251f37ae8159cd7dc4b6b3b2a5b0
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-06-24 09:51:23 +0500

    loc-date-fix

--- coin/src/js/utils/format-date.js
+++ coin/src/js/utils/format-date.js
@@ -1,5 +1,16 @@
 export default function formatDate(inputDate) {
   const date = new Date(inputDate);
-  const options = { day: 'numeric', month: 'long', year: 'numeric' };
-  return date.toLocaleDateString('ru-RU', options);
+  return `${date.getDate()} ${getMonthName(date.getMonth())} ${date.getFullYear()} г.`;
 }
+
+function getMonthName(monthIndex) {
+  const monthNames = ["января", "февраля", "марта", "апреля", "мая", "июня",
+                     "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+  return monthNames[monthIndex];
+}
+
+// export default function formatDate(inputDate) {
+//   const date = new Date(inputDate);
+//   const options = { day: 'numeric', month: 'long', year: 'numeric' };
+//   return date.toLocaleDateString(navigator.language, options); 
+// }
